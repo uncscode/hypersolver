6a4c2157cebc20c073a1995cf12f27c35f856465
ngam <67342040+ngam@users.noreply.github.com>
2022-07-28 16:33:01 +0000

    further custom

--- hypersolver/lax_friedrichs.py
+++ hypersolver/lax_friedrichs.py
@@ -1,11 +1,13 @@
 """ Lax-Friedrics scheme """
 
-# import numpy as np
+import numpy as np
+
+from hypersolver.util import term_util
 
 
 def next_step(
     this_step,
-    size_step,
+    vars_vals,
     time_step,
     flux_term,
     sink_term
@@ -16,47 +18,81 @@ def next_step(
 
     result[1:-1] = (
         0.5 * (this_step[2:] + this_step[:-2]) -
-        0.5 * (this_step[2:] - this_step[:-2]) *
-        flux_term * time_step / size_step +
-        sink_term * time_step
+        1.0 * (this_step[2:] - this_step[:-2]) *
+        flux_term[1:-1] * time_step / (vars_vals[2:] - vars_vals[:-2]) +
+        sink_term[1:-1] * time_step
     )
 
     result[0] = (
         0.5 * (this_step[1] + this_step[0]) -
-        0.5 * (this_step[1] - this_step[0]) *
-        flux_term * time_step / size_step +
-        sink_term * time_step
+        1.0 * (this_step[1] - this_step[0]) *
+        flux_term[0] * time_step / (vars_vals[1] - vars_vals[0]) +
+        sink_term[0] * time_step
     )
 
     result[-1] = (
         0.5 * (this_step[-1] + this_step[-2]) -
-        0.5 * (this_step[-1] - this_step[-2]) *
-        flux_term * time_step / size_step +
-        sink_term * time_step
+        1.0 * (this_step[-1] - this_step[-2]) *
+        flux_term[-1] * time_step / (vars_vals[-1] - vars_vals[-2]) +
+        sink_term[-1] * time_step
     )
 
     return result
 
 
-def solve(
-    initial_condition,
-    size_step,
-    time_step,
+def solver(
+    init_vals,
+    vars_vals,
+    time_span,
     flux_term,
-    sink_term
+    sink_term,
+    **kwargs
 ):
     """ Lax-Friedrics scheme """
 
-    result = initial_condition
+    stability_factor = kwargs.get('stability_factor', 0.8)
+    verbosity = kwargs.get('verbosity', 0)
 
-    for _ in range(1000):
+    vars_vals = term_util(vars_vals, init_vals)
+
+    if isinstance(flux_term, type(next_step)):
+        flux_term = flux_term(init_vals, vars_vals, **kwargs)
+    if isinstance(sink_term, type(next_step)):
+        sink_term = flux_term(init_vals, vars_vals, **kwargs)
+    flux_term = term_util(flux_term, init_vals)
+    sink_term = term_util(sink_term, init_vals)
+
+    time_step = (
+        stability_factor *
+        np.diff(vars_vals).min() /
+        np.abs(flux_term).max()
+    )
+
+    tidx = np.arange(time_span[0], time_span[-1]+time_step, time_step)
+    sols = np.zeros((tidx.size, init_vals.size))
+    itrs = 0
+    sols[itrs] = init_vals
+
+    for idx in tidx[:-1]:
         next_vals = next_step(
-            result,
-            size_step,
+            sols[itrs],
+            vars_vals,
             time_step,
             flux_term,
             sink_term
         )
-        result = next_vals
 
-    return result
+        if verbosity == 1:
+            print(idx, itrs)
+
+        if isinstance(flux_term, type(next_step)):
+            flux_term = flux_term(sols[itrs], vars_vals, **kwargs)
+        if isinstance(sink_term, type(next_step)):
+            sink_term = flux_term(sols[itrs], vars_vals, **kwargs)
+        flux_term = term_util(flux_term, sols[itrs])
+        sink_term = term_util(sink_term, sols[itrs])
+
+        itrs += 1
+        sols[itrs] = next_vals
+
+    return sols
