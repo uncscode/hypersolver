2ea46fa1db99b46dabc988e6954305036a9e541a
ngam <67342040+ngam@users.noreply.github.com>
2022-07-30 02:10:47 +0000

    updates

--- hypersolver/util.py
+++ hypersolver/util.py
@@ -13,7 +13,6 @@ def term_util(
         this utility "regularizes" the input "term"
         by making it look like the "orig" input
     """
-    print(type(term))
     if isinstance(term, np.ndarray) and np.array(term).shape == orig.shape:
         return term
     return np.full_like(orig, term, dtype=np.float_)
