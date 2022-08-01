61734d421c0157ea7ecb7323f19e691a959d52cc
ngam <67342040+ngam@users.noreply.github.com>
2022-08-01 00:56:17 +0000

    wip revamp moc to do next step only

--- hypersolver/method_of_characteristics.py
+++ hypersolver/method_of_characteristics.py
@@ -6,13 +6,12 @@ from scipy.integrate import odeint
 from hypersolver.accurate_derivative import acc_derivative
 
 
-def moc(
+def moc_next(
     init_vals,
     vars_vals,
-    time_span,
     flux_term,
     sink_term,
-    **kwargs
+    time_step,
 ):
     """ method of characteristics
 
@@ -29,20 +28,18 @@ def moc(
         ------
         init_step:  n
         vars_vals:  x
-        time_span:
         flux_term:  f
         sink_term:  g
+        time_step:
 
         outputs
         -------
-        next_vars:  x
         next_vals:  n
 
         numerics
         --------
         use from scipy.integrate.odeint
     """
-    _ = kwargs
 
     # pylint: disable=unused-argument
     # pylint: disable=unused-variable
@@ -66,7 +63,7 @@ def moc(
     yval0 = np.empty((vars_vals.size + init_vals.size))
     yval0[::2] = vars_vals
     yval0[1::2] = init_vals
-    tspan = np.linspace(time_span[0], time_span[-1], 100)
+    tspan = np.linspace(0, time_step, 10)
     results = odeint(_func, yval0, tspan, ml=2, mu=2)
 
     return (results[:, ::2], results[:, 1::2])
