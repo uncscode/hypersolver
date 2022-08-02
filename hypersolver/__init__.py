1aedc773d60a646d561b4477a1cde8428c4ffd46
ngam <67342040+ngam@users.noreply.github.com>
2022-08-02 19:18:46 +0000

    add ode solver

--- hypersolver/__init__.py
+++ hypersolver/__init__.py
@@ -21,40 +21,66 @@
     >>> solver(n0, x, t, f, g, **kwargs)
     >>> # kwargs include "method", "backend", etc.
 
-    available methods:
+    available `method`s:
+    pde:
         - "lax_friedrichs" (default)
         - "lax_wendroff"
         - "method_of_characteristics" (broken, experimental)
+    ode:
+        - "rk2"
 
-    available backends:
+    available `backend`s:
         - "numpy" (default)
         - "jax" (experimental)
+
+    available `solver_type`s:
+        - "unsplit" (default)
+        - "split"
 """
 
 import os
 
-from hypersolver.pde_solver_unsplit import solver_
+from hypersolver.pde_solver_unsplit import solver_ as solver_upde
+# from hypersolver.pde_solver_split import solver_ as solver_spde
+from hypersolver.ode_solver import solver_ as solver_ode
 
 
 __version__ = "0.0.5"
 
-__hyper_solvers__ = [
+__hyper_methods__ = [
     "lax_friedrichs",
     "lax_wendroff",
     "method_of_characteristics",
+    "rk2",
 ]
+
+__hyper_solver_types__ = ["unsplit", "split",]
 
 
 def solver(
     *args,
     method="lax_friedrichs",
     backend=os.environ.get("BACKEND", "numpy"),
+    verbosity=os.environ.get("VERBOSITY", "0"),
+    solver_type="unsplit",
     **kwargs
 ):
     """ wrapper function to select solvers """
 
-    os.environ["BACKEND"] = backend
+    os.environ["BACKEND"] = str(backend)
+    os.environ["VERBOSITY"] = str(verbosity)
 
-    if method not in __hyper_solvers__:
+    if method not in __hyper_methods__ or \
+            solver_type not in __hyper_solver_types__:
         raise ValueError("method not supported")
-    return solver_(*args, method=method, **kwargs)
+
+    if method.startswith("rk"):
+        return solver_ode(*args, method=method, **kwargs)
+    # if method.endswith("_split"):
+    #     return solver_spde(*args, method=method, **kwargs)
+    return solver_upde(
+        *args,
+        method=method,
+        verbosity=verbosity,
+        **kwargs
+    )
--- hypersolver/pde_solver_unsplit.py
+++ hypersolver/pde_solver_unsplit.py
@@ -12,79 +12,82 @@ if os.environ.get("BACKEND", "numpy") == "jax":
 
 def solver_(*args, **kwargs):
     """ set the solver """
+
     method = kwargs.get("method", "lax_friedrichs")
-    if method == "lax_friedrichs":
-        next_step = lx_next
-    else:
-        next_step = lw_next
+
+    next_step = lx_next if method == "lax_friedrichs" else lw_next
+
     if os.environ.get("BACKEND", "numpy") == "jax":
         next_step = jax.jit(next_step)
 
-    def _solver_(  # pylint: disable=too-many-locals
-        init_vals,
-        vars_vals,
-        time_span,
-        flux_term,
-        sink_term,
-        **kwargs
+    def _prep_solver(
+        init_vals, vars_vals, time_span, flux_term, sink_term, **kwargs
     ):
-        """ solver accorrding to finite-difference schemes """
+        """ prep solver """
 
         stability_factor = kwargs.get('stability_factor', 0.98)
-        verbosity = kwargs.get('verbosity', 0)
 
         vars_vals = term_util(vars_vals, init_vals)
 
         _flux_term = term_util(
-            func_util(flux_term, init_vals, vars_vals, **kwargs),
-            init_vals)
+            func_util(flux_term, init_vals, vars_vals, **kwargs), init_vals)
+
         _sink_term = term_util(
-            func_util(sink_term, init_vals, vars_vals, **kwargs),
-            init_vals)
+            func_util(sink_term, init_vals, vars_vals, **kwargs), init_vals)
 
         stability_factor, time_step = (
             stability_factor,
             time_step_util(vars_vals, _flux_term, stability_factor)
-        ) if method in [
-            "lax_friedrichs", "lax_wendroff"
-        ] else (
-            np.array((time_span[-1] - time_span[0])/5.0),
-            np.array((time_span[-1] - time_span[0])/5.0))
+        ) if method in ["lax_friedrichs", "lax_wendroff"] else (
+            np.array((time_span[-1] - time_span[0]) / 5.0),
+            np.array((time_span[-1] - time_span[0]) / 5.0))
 
-        tidx = np.arange(time_span[0], time_span[-1]+time_step, time_step)
+        tidx = np.arange(time_span[0], time_span[-1] + time_step, time_step)
+
         itrs = 0
+
         sols = init_vals.reshape(1, -1)
 
         if method == "lax_wendroff":
-            _sink_term = (_sink_term, _sink_term)
+            _sink_term = _sink_term, _sink_term
 
-        for idx in range(tidx[:-1].size):
+        return (
+            tidx, itrs, sols, stability_factor,
+            _flux_term, _sink_term
+        )
+
+    def _solver_(init_vals, vars_vals, time_span, flux_term, sink_term, **kwargs):
+        """ solver accorrding to finite-difference schemes """
+
+        (
+            tidx, itrs, sols, stability_factor,
+            _flux_term, _sink_term
+        ) = _prep_solver(
+            init_vals, vars_vals, time_span, flux_term, sink_term, **kwargs
+        )
+
+        for _ in range(tidx[:-1].size):
+
             next_vals = next_step(
-                sols[itrs, :],
-                vars_vals,
-                _flux_term,
-                _sink_term,
-                stability_factor,
-            )
+                sols[itrs, :], vars_vals, _flux_term, _sink_term, stability_factor)
 
-            if verbosity == 1:
-                print(itrs, idx)
+            if os.environ.get("VERBOSITY", "0") == "1":
+                print(itrs)
 
             _flux_term = term_util(
-                func_util(flux_term, sols[itrs, :], vars_vals, **kwargs),
-                sols[itrs, :])
+                func_util(flux_term, sols[itrs, :], vars_vals, **kwargs), sols[itrs, :])
+
             _sink_term_ = term_util(
-                func_util(sink_term, sols[itrs, :], vars_vals, **kwargs),
-                sols[itrs, :])
+                func_util(sink_term, sols[itrs, :], vars_vals, **kwargs), sols[itrs, :])
+
             if method == "lax_wendroff":
-                _sink_term = (_sink_term[1], _sink_term_)
+                _sink_term = _sink_term[1], _sink_term_
             else:
                 _sink_term = _sink_term_
 
             itrs += 1
-            _sols = np.concatenate([sols, next_vals.reshape(1, -1)], axis=0)
-            del sols
-            sols = _sols
+
+            sols = np.concatenate([sols, next_vals.reshape(1, -1)], axis=0)
 
         return sols
 
