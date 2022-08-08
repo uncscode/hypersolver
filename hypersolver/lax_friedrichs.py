34c4d1b9db59260a46b32194de40ab3f3a905dce
ngam <67342040+ngam@users.noreply.github.com>
2022-08-08 01:08:50 +0000

    move loop away

--- hypersolver/lax_friedrichs.py
+++ hypersolver/lax_friedrichs.py
@@ -2,6 +2,7 @@
 
 from hypersolver.util import xnp as np
 from hypersolver.util import jxt as jit
+from hypersolver.util import time_step_util
 from hypersolver.derivative import ord1_acc2
 
 
@@ -46,3 +47,43 @@ def lx_next(
 
     return _init_vals - time_step * (
         ord1_acc2(init_vals*flux_term, vars_vals) - sink_term)
+
+
+@jit(nopython=True)
+def lx_loop(time, init_vals, vars_vals, _flux_term, _sink_term, stability):
+    """ loop over """
+
+    time_step = time_step_util(
+        vars_vals, _flux_term(init_vals, vars_vals), stability
+    )
+
+    tidx = np.arange(
+        time[0], time[-1] + time_step, time_step
+    )
+
+    pts = np.asarray((tidx.size+1., np.asarray(time).size+100., 100.)).min()//1
+    print(pts, tidx.size, np.asarray(time).size)
+    sols = np.asarray(init_vals).reshape(1, -1)
+    tims = np.asarray(tidx[0]).reshape(1, -1)
+    _yvar = sols[0]
+
+    for itrs in range(tidx[:-1].size):
+
+        next_vals = lx_next(
+            _yvar,
+            vars_vals,
+            _flux_term(_yvar, vars_vals),
+            _sink_term(_yvar, vars_vals),
+            tidx[itrs + 1] - tidx[itrs]
+        )
+
+        if ((itrs + 1) % (tidx.size//pts)) == 0:
+            sols = np.concatenate(
+                (sols,
+                 np.asarray(next_vals).reshape(1, -1)), axis=0)
+            tims = np.concatenate(
+                (tims,
+                 np.asarray(tidx[itrs]).reshape(1, -1)), axis=0)
+        _yvar = next_vals
+
+    return tims, sols
--- hypersolver/tests/lax_friedrichs_test.py
+++ hypersolver/tests/lax_friedrichs_test.py
@@ -1,7 +1,8 @@
 """ test: Lax-Friedrichs finite-difference scheme """
 
 from hypersolver.util import xnp as np
-from hypersolver.lax_friedrichs import lx_next
+from hypersolver.util import jxt as jit
+from hypersolver.lax_friedrichs import lx_next, lx_loop
 
 
 def test_lx_next():
@@ -20,3 +21,24 @@ def test_lx_next():
     assert lx_next(
         _array, _array, 1.0, 0.0, 0.01
     ).shape == (_array.size,)
+
+
+def test_lx_loop():
+    """ test: loop for lx scheme """
+
+    xvar = np.linspace(1, 10, 100)
+    yvar = 1.0 * (xvar > 4) - 1.0 * (xvar > 6)
+
+    @jit(nopython=True)
+    def flux(yvar, xvar):  # pylint: disable=unused-argument
+        """ flux """
+        return 5 / xvar
+
+    @jit(nopython=True)
+    def sink(yvar, xvar):  # pylint: disable=unused-argument
+        """ sink """
+        return -0.01 * yvar
+
+    time = np.linspace(0, 2, 1000)
+    assert (lx_loop(time, yvar.reshape(1, -1), xvar,
+            flux, sink, 0.9))[-1].shape[0] >= 100
