cca4b9eefc3ad85c4169b1f44470295ff2ecd01c
ngam <67342040+ngam@users.noreply.github.com>
2022-08-21 15:08:46 +0000

    add ord2acc4

--- hypersolver/tests/derivative_test.py
+++ hypersolver/tests/derivative_test.py
@@ -5,7 +5,7 @@ import pytest
 
 from hypersolver.util import xnp as np
 from hypersolver.derivative import ord1_acc2, ord2_acc2
-from hypersolver.derivative import ord1_acc4
+from hypersolver.derivative import ord1_acc4, ord2_acc4
 
 xvar = np.linspace(0, 10, 1000)
 yvar = np.sin(xvar)
@@ -75,3 +75,31 @@ def test_ord2_acc2():
         assert (ord2_acc2(
             yvar, xvar) - d2ydx2).sum() == pytest.approx(
                 (d2ydx2-d2ydx2).sum(), abs=1e-1)
+
+
+def test_ord2_acc4():
+    """ test: central differncing: order=2, accuracy=2 """
+
+    assert ord2_acc4(
+        yvar, xvar).shape == d2ydx2.shape
+
+    assert ord2_acc4(
+        yvar, xvar) == pytest.approx(d2ydx2, abs=1e-1)
+
+    if os.environ.get("HS_BACKEND", "numpy") == "numpy":
+        assert (ord2_acc4(
+            yvar, xvar) - d2ydx2).sum() == pytest.approx(0.0, abs=1e-1)
+    else:
+        assert (ord2_acc4(
+            yvar, xvar) - d2ydx2).sum() == pytest.approx(
+                (d2ydx2-d2ydx2).sum(), abs=1e-1)
+
+
+def test_comp_ord2():
+    """ test: compare ord1 acc(2,4) """
+
+    assert (
+        (ord2_acc4(yvar, xvar)-d2ydx2)**2
+    ).sum() <= (
+        (ord2_acc2(yvar, xvar)-d2ydx2)**2
+    ).sum()
